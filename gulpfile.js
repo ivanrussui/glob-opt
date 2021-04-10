e09c8ba2ea5bc4671ba8239c485259ab492538a4
Ivan <fenomen_ny@mail.ru>
2021-04-11 00:48:27 +0300

    header

--- gulpfile.js
+++ gulpfile.js
@@ -33,7 +33,7 @@ gulp.task('watch', function() {
     gulp.watch("src/*.html").on('change', gulp.parallel('html'));
     gulp.watch("src/js/**/*.js").on('change', gulp.parallel('scripts'));
     gulp.watch("src/fonts/**/*").on('all', gulp.parallel('fonts'));
-    gulp.watch("src/icons/**/*").on('all', gulp.parallel('icons'));
+    // gulp.watch("src/icons/**/*").on('all', gulp.parallel('icons'));
     gulp.watch("src/img/**/*").on('all', gulp.parallel('images'));
 });
 
@@ -55,11 +55,11 @@ gulp.task('fonts', function () {
         .pipe(browserSync.stream());
 });
 
-gulp.task('icons', function () {
-    return gulp.src("src/icons/**/*")
-        .pipe(gulp.dest("dist/icons"))
-        .pipe(browserSync.stream());
-});
+// gulp.task('icons', function () {
+//     return gulp.src("src/icons/**/*")
+//         .pipe(gulp.dest("dist/icons"))
+//         .pipe(browserSync.stream());
+// });
 
 gulp.task('images', function () {
     return gulp.src("src/img/**/*")
@@ -68,4 +68,5 @@ gulp.task('images', function () {
         .pipe(browserSync.stream());
 });
 
-gulp.task('default', gulp.parallel('watch', 'server', 'styles', 'scripts', 'fonts', 'icons', 'html', 'images'));
+// gulp.task('default', gulp.parallel('watch', 'server', 'styles', 'scripts', 'fonts', 'icons', 'html', 'images'));
+gulp.task('default', gulp.parallel('watch', 'server', 'styles', 'scripts', 'fonts', 'html', 'images'));
