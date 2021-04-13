fee342d77f4b59e379ed4175ac3abdf95085b142
Ivan <fenomen_ny@mail.ru>
2021-04-13 19:59:55 +0300

    burger

--- src/js/script.js
+++ src/js/script.js
@@ -1,4 +1,15 @@
 // burger
 const hamburger = document.querySelector('.hamburger'),
-	navMenu = document.querySelector('.navbar__menu'),
+	menu = document.querySelector('.menu'),
+	// menu = document.querySelector('.navbar__menu', '.menu__close'),
+	menuClose = document.querySelector('.menu__close'),
 	bodyLock = document.querySelector('body');
+
+hamburger.addEventListener('click', () => {
+	menu.classList.add('active');
+	// menuClose.classList.add('active');
+});
+
+menuClose.addEventListener('click', () => {
+	menu.classList.remove('active');
+});
