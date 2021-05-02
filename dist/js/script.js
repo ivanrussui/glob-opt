f87365545747880f790b80b23a0fd6520a56a794
Ivan <fenomen_ny@mail.ru>
2021-05-03 02:49:20 +0300

    cleaner

--- dist/js/script.js
+++ dist/js/script.js
@@ -17,17 +17,10 @@ menuClose.addEventListener('click', () => {
 
 // Swiper
 const swiper = new Swiper('.swiper-container', {
-  // Optional parameters
-  // direction: 'horizontal',
   loop: true,
   slidesPerView: 1,
 	centeredSlides: true,
 	spaceBetween: 130,
-
-  // If we need pagination
-  // pagination: {
-  //   el: '.swiper-pagination',
-  // },
 
   // Navigation arrows
   navigation: {
@@ -35,89 +28,9 @@ const swiper = new Swiper('.swiper-container', {
     prevEl: '.swiper-button-prev',
   },
 
-  // And if we need scrollbar
-  // scrollbar: {
-  //   el: '.swiper-scrollbar',
-  // },
   breakpoints: {
-    // 768: {
-    //   slidesPerView: 2,
-    // },
     1200: {
       slidesPerView: 3,
     },
   },
 });
-
-// Tiny Slider
-// const slider = tns({
-//   container: '.reviews__slider',
-//   items: 1,
-// 	centerMode: true,
-//   slideBy: 'page',
-//   controls: false,
-//   autoplay: false,
-//   autoplayHoverPause: true,
-//   autoplayButtonOutput: false,
-//   navPosition: 'bottom',
-//   speed: 600,
-//   responsive: {
-//     320: {
-//       nav: true,
-//     },
-//     768: {
-//       nav: false,
-//     },
-//   },
-// });
-
-// переключение кнопок в слайдере
-// document.querySelector('.prev').addEventListener('click', function () {
-// 	slider.goTo('prev');
-// });
-// document.querySelector('.next').addEventListener('click', function () {
-// 	slider.goTo('next');
-// });
-
-// img hover
-// let priceBlock = document.querySelector('.price__block'),
-//   imgOrigin = document.querySelector('.price__img_origin'),
-//   imgHover = document.querySelector('.price__img_hover'),
-//   priceImg = document.querySelectorAll('.price__img');
-
-// console.log(priceBlock);
-
-// for (let i = 0; i < priceBlock.length; i++) {
-// 	priceBlock[i].addEventListener('mouseenter', function () {
-// 		imgOrigin.style.display = 'none';
-// 		imgHover.style.display = 'block';
-//   });
-// }
-
-// for (let i = 0; i < priceBlock.length; i++) {
-//   priceBlock[i].addEventListener('mouseleave', function () {
-// 		imgOrigin.style.display = 'block';
-// 		imgHover.style.display = 'none';
-//   });
-// }
-// for (let i = 0; i < priceBlock.length; i++) {
-//   priceBlock[i].addEventListener('mouseenter', function () {
-//     imgOrigin.style.display = 'none';
-//     imgHover.style.display = 'block';
-//   });
-// }
-// for (let i = 0; i < priceBlock.length; i++) {
-//   priceBlock[i].addEventListener('mouseleave', function () {
-//     imgOrigin.style.display = 'block';
-//     imgHover.style.display = 'none';
-//   });
-// }
-
-// priceBlock.addEventListener('mouseenter', () => {
-// 	imgOrigin.style.display = 'none';
-// 	imgHover.style.display = 'block';
-// });
-// priceBlock.addEventListener('mouseleave', () => {
-// 	imgOrigin.style.display = 'block';
-// 	imgHover.style.display = 'none';
-// });
